af4e917d5f1f3091bfcf2ced088826e4eccce2e0
hamza benidar <benidar.hamza@gmail.com>
2022-06-07 17:05:13 +0200

    add timeline, card event  and history page

---
+++ frontend/src/pages/History/style.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+export default styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 75vw;
+  margin: auto;
+  @media screen and (max-width: 700px) {
+    width: 90vw;
+  }
+`;
