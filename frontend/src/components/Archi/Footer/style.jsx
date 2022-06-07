05627522f8b91e0c95c660c3390e13cd17ac37d7
romainLF <romainlefloch@hotmail.com>
2022-06-07 16:22:28 +0200

    footer stayed down

--- frontend/src/components/Archi/Footer/style.jsx
+++ frontend/src/components/Archi/Footer/style.jsx
@@ -4,8 +4,6 @@ export default styled.footer`
   background-color: var(--yellow);
   height: 50vh;
   width: 100vw;
-  position: absolute;
-  bottom: 0;
   overflow: hidden;
   display: flex;
   .imgPosition {
