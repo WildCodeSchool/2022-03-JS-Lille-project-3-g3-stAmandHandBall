d4ac0b62c39f150cf4d69bb6fdec8206b50b6208
NtGZeko <matheooz@live.fr>
2022-06-14 12:28:13 +0200

    add route for link need fix link Rookies

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,6 +10,20 @@ import StaffList from "@pages/StaffList";
 import Contact from "@pages/Contact";
 import Calendar from "@pages/Calendar";
 import History from "@pages/History";
+import OrigineDeLaMeute from "@pages/OrigineDeLaMeute";
+import ChefDeLaMeute from "@pages/ChefDeLaMeute";
+import BergersDesLouveteaux from "@pages/BergersDesLouveteaux";
+import Arbitre from "@pages/Arbitre";
+import LesLouves from "@pages/LesLouves";
+import Calendrier from "@pages/Calendrier";
+import Classement from "@pages/Classement";
+import Philosophie from "@pages/Philosophie";
+import Effectif from "@pages/Effectif";
+import VieQuotidienne from "@pages/VieQuotidienne";
+import Formation from "@pages/Formation";
+import Candidature from "@pages/Candidature";
+import Ambition from "@pages/Ambition";
+import Partenaire from "@pages/Partenaire";
 
 function App() {
   return (
@@ -27,6 +41,23 @@ function App() {
           <Route path="/contact" element={<Contact />} />
           <Route path="/calendrier" element={<Calendar />} />
           <Route path="/histoire" element={<History />} />
+          <Route path="origineDeLaMeute" element={<OrigineDeLaMeute />} />
+          <Route path="chefsDeLaMeute" element={<ChefDeLaMeute />} />
+          <Route
+            path="bergersDesLouveteaux"
+            element={<BergersDesLouveteaux />}
+          />
+          <Route path="arbitres" element={<Arbitre />} />
+          <Route path="lesLouves" element={<LesLouves />} />
+          <Route path="calendrier" element={<Calendrier />} />
+          <Route path="classement" element={<Classement />} />
+          <Route path="philosophie" element={<Philosophie />} />
+          <Route path="effectif" element={<Effectif />} />
+          <Route path="vieQuotidienne" element={<VieQuotidienne />} />
+          <Route path="formation" element={<Formation />} />
+          <Route path="candidature" element={<Candidature />} />
+          <Route path="ambition" element={<Ambition />} />
+          <Route path="partenaires" element={<Partenaire />} />
         </Routes>
       </main>
       <Footer />
