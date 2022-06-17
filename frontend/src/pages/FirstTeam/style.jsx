b9a02032dc3f6da19500c2b92d597e519fd5336c
NtGZeko <matheooz@live.fr>
2022-06-17 12:03:29 +0200

    finish responsive page equipe i center block title

--- frontend/src/pages/FirstTeam/style.jsx
+++ frontend/src/pages/FirstTeam/style.jsx
@@ -7,5 +7,8 @@ export default styled.div`
   margin: auto;
   @media screen and (max-width: 700px) {
     width: 90vw;
+    display: flex;
+    align-items: center;
+    justify-content: center;
   }
 `;
