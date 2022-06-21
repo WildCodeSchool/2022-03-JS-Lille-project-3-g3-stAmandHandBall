8c409abb45e2a1bd73e1589d2078d5bfb05f2d35
NtGZeko <matheooz@live.fr>
2022-06-21 16:17:49 +0200

    creat first route (player) is OK

---
+++ backend/src/controllers/PlayerController.js
@@ -0,0 +1,83 @@
+const models = require("../models");
+
+class PlayerController {
+  static browse = (req, res) => {
+    models.player
+      .findAll()
+      .then(([rows]) => {
+        res.send(rows);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.sendStatus(500);
+      });
+  };
+
+  static read = (req, res) => {
+    models.player
+      .find(req.params.id)
+      .then(([rows]) => {
+        if (rows[0] == null) {
+          res.sendStatus(404);
+        } else {
+          res.send(rows[0]);
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        res.sendStatus(500);
+      });
+  };
+
+  static edit = (req, res) => {
+    const player = req.body;
+
+    // TODO validations (length, format...)
+
+    player.id = parseInt(req.params.id, 10);
+
+    models.player
+      .update(player)
+      .then(([result]) => {
+        if (result.affectedRows === 0) {
+          res.sendStatus(404);
+        } else {
+          res.sendStatus(204);
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        res.sendStatus(500);
+      });
+  };
+
+  static add = (req, res) => {
+    const player = req.body;
+
+    // TODO validations (length, format...)
+
+    models.player
+      .insert(player)
+      .then(([result]) => {
+        res.status(201).send({ ...player, id: result.insertId });
+      })
+      .catch((err) => {
+        console.error(err);
+        res.sendStatus(500);
+      });
+  };
+
+  static delete = (req, res) => {
+    models.player
+      .delete(req.params.id)
+      .then(() => {
+        res.sendStatus(204);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.sendStatus(500);
+      });
+  };
+}
+
+module.exports = PlayerController;
--- backend/src/router.js
+++ backend/src/router.js
@@ -1,10 +1,18 @@
 const express = require("express");
 
-const { ItemController } = require("./controllers");
+const { ItemController, PlayerController } = require("./controllers");
 
 const router = express.Router();
 
+router.get("/players", PlayerController.browse);
+router.get("/players/:id", PlayerController.browse);
+router.get("/players/:id", PlayerController.read);
+router.put("/players/:id", PlayerController.edit);
+router.post("/players", PlayerController.add);
+router.delete("/players/:id", PlayerController.delete);
+
 router.get("/items", ItemController.browse);
+router.get("/items/:id", ItemController.browse);
 router.get("/items/:id", ItemController.read);
 router.put("/items/:id", ItemController.edit);
 router.post("/items", ItemController.add);
