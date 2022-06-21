51900d470f4d1f5909ae1cc1bf0b7a409748092c
Gonaline <aline-gonce_student2022@wilder.school>
2022-06-21 16:18:21 +0200

    create players routes

--- backend/src/router.js
+++ backend/src/router.js
@@ -1,8 +1,14 @@
 const express = require("express");
 
-const { ItemController } = require("./controllers");
+const { ItemController, PlayerController } = require("./controllers");
 
 const router = express.Router();
+
+router.get("/players", PlayerController.browse);
+router.get("/players/:id", PlayerController.read);
+router.put("/players/:id", PlayerController.edit);
+router.post("/players", PlayerController.add);
+router.delete("/players/:id", PlayerController.delete);
 
 router.get("/items", ItemController.browse);
 router.get("/items/:id", ItemController.read);
