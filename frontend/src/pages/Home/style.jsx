77a26aae9102f2df2d9bf42be32d5790c5cab1e8
hamza benidar <benidar.hamza@gmail.com>
2022-06-22 15:40:24 +0200

    delete dead style

--- frontend/src/pages/Home/style.jsx
+++ frontend/src/pages/Home/style.jsx
@@ -4,12 +4,4 @@ export default styled.div`
   main {
     background-color: #18399d;
   }
-
-  .compteur {
-    display: flex;
-    justify-content: center;
-    border: 3px solid red;
-    padding: 2px;
-    color: var(--white);
-  }
 `;
