c1fc53b6a27455a8c8d33d41f03bcb310adf9bf5
Gonaline <aline-gonce_student2022@wilder.school>
2022-07-05 12:34:24 +0200

    create rookies routes

---
+++ backend/src/controllers/TeamController.js
@@ -0,0 +1,83 @@
+const models = require("../models");
+
+class TeamController {
+  static browse = (req, res) => {
+    models.team
+      .findAll()
+      .then(([rows]) => {
+        res.send(rows);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.sendStatus(500);
+      });
+  };
+
+  static read = (req, res) => {
+    models.team
+      .find(req.params.id)
+      .then(([rows]) => {
+        if (rows[0] == null) {
+          res.sendStatus(404);
+        } else {
+          res.send(rows[0]);
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        res.sendStatus(500);
+      });
+  };
+
+  static edit = (req, res) => {
+    const team = req.body;
+
+    // TODO validations (length, format...)
+
+    team.id = parseInt(req.params.id, 10);
+
+    models.team
+      .update(team)
+      .then(([result]) => {
+        if (result.affectedRows === 0) {
+          res.sendStatus(404);
+        } else {
+          res.sendStatus(204);
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        res.sendStatus(500);
+      });
+  };
+
+  static add = (req, res) => {
+    const team = req.body;
+
+    // TODO validations (length, format...)
+
+    models.team
+      .insert(team)
+      .then(([result]) => {
+        res.status(201).send({ ...team, id: result.insertId });
+      })
+      .catch((err) => {
+        console.error(err);
+        res.sendStatus(500);
+      });
+  };
+
+  static delete = (req, res) => {
+    models.team
+      .delete(req.params.id)
+      .then(() => {
+        res.sendStatus(204);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.sendStatus(500);
+      });
+  };
+}
+
+module.exports = TeamController;
--- backend/src/router.js
+++ backend/src/router.js
@@ -1,6 +1,10 @@
 const express = require("express");
 
-const { PlayerController, StaffController } = require("./controllers");
+const {
+  PlayerController,
+  StaffController,
+  TeamController,
+} = require("./controllers");
 
 const router = express.Router();
 
@@ -15,5 +19,11 @@ router.get("/staffs/:id", StaffController.read);
 router.put("/staffs/:id", StaffController.edit);
 router.post("/staffs", StaffController.add);
 router.delete("/staffs/:id", StaffController.delete);
+
+router.get("/team", TeamController.browse);
+router.get("/team/:id", TeamController.read);
+router.put("/team/:id", TeamController.edit);
+router.post("/team", TeamController.add);
+router.delete("/team/:id", TeamController.delete);
 
 module.exports = router;
