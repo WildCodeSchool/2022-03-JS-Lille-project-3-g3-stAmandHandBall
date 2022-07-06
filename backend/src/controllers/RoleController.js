b7caaa90eb8731ffc39d9f020ef1fdbf54f69bf1
NtGZeko <matheooz@live.fr>
2022-07-06 10:46:15 +0200

    I have created the role table, and created a join between the staff route and the role roule using staff_has_role

---
+++ backend/src/controllers/RoleController.js
@@ -0,0 +1,95 @@
+const models = require("../models");
+
+class RoleController {
+  static browse = (req, res) => {
+    models.role
+      .findAll()
+      .then(([rows]) => {
+        res.send(rows);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.sendStatus(500);
+      });
+  };
+
+  static roleAndStaff = (req, res) => {
+    models.role
+      .findAllStaffAndRole()
+      .then(([rows]) => {
+        res.send(rows);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.sendStatus(500);
+      });
+  };
+
+  static read = (req, res) => {
+    models.role
+      .find(req.params.id)
+      .then(([rows]) => {
+        if (rows[0] == null) {
+          res.sendStatus(404);
+        } else {
+          res.send(rows[0]);
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        res.sendStatus(500);
+      });
+  };
+
+  static edit = (req, res) => {
+    const role = req.body;
+
+    // TODO validations (length, format...)
+
+    role.id = parseInt(req.params.id, 10);
+
+    models.role
+      .update(role)
+      .then(([result]) => {
+        if (result.affectedRows === 0) {
+          res.sendStatus(404);
+        } else {
+          res.sendStatus(204);
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        res.sendStatus(500);
+      });
+  };
+
+  static add = (req, res) => {
+    const role = req.body;
+
+    // TODO validations (length, format...)
+
+    models.role
+      .insert(role)
+      .then(([result]) => {
+        res.status(201).send({ ...role, id: result.insertId });
+      })
+      .catch((err) => {
+        console.error(err);
+        res.sendStatus(500);
+      });
+  };
+
+  static delete = (req, res) => {
+    models.role
+      .delete(req.params.id)
+      .then(() => {
+        res.sendStatus(204);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.sendStatus(500);
+      });
+  };
+}
+
+module.exports = RoleController;
