528275b36dad154aca4837abb3bc3f1afb0cb377
Gonaline <aline-gonce_student2022@wilder.school>
2022-07-06 17:11:06 +0200

    add luxon

--- frontend/src/services/context/Ctx.jsx
+++ frontend/src/services/context/Ctx.jsx
@@ -6,7 +6,6 @@ const ctxProvider = createContext();
 export default ctxProvider;
 
 export function CtxProvider({ children }) {
-  const [test, setTest] = useState();
   const [players, setPlayers] = useState([]);
   const [calendar, setCalendar] = useState([]);
 
@@ -30,8 +29,6 @@ export function CtxProvider({ children }) {
     <ctxProvider.Provider
       // eslint-disable-next-line react/jsx-no-constructed-context-values
       value={{
-        test,
-        setTest,
         players,
         calendar,
       }}
