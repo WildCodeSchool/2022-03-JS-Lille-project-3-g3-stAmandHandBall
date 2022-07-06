374b17cb160b3ded492cc2f9fb106fc0147bff11
NtGZeko <matheooz@live.fr>
2022-07-06 11:56:52 +0200

    delete dead code abstract manager

--- backend/src/models/AbstractManager.js
+++ backend/src/models/AbstractManager.js
@@ -14,12 +14,6 @@ class AbstractManager {
     return this.connection.query(`select * from  ${this.table}`);
   }
 
-  findAllStaffAndRole() {
-    return this.connection.query(
-      `SELECT * FROM staff_has_role AS shr JOIN staff AS s ON s.id=shr.staff_id JOIN role AS r ON r.id=shr.role_id;`
-    );
-  }
-
   delete(id) {
     return this.connection.query(`delete from ${this.table} where id = ?`, [
       id,
