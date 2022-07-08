3bd414af07a633c93572913ad59e5fb5bab32185
NtGZeko <matheooz@live.fr>
2022-07-08 11:53:50 +0200

    create TeamControler, RefereeController, OpponentController, AdminController

---
+++ backend/src/controllers/OpponentController.js
@@ -0,0 +1,78 @@
+const models = require("../models");
+
+class OpponentController {
+  static browse = (req, res) => {
+    models.Opponent.findAll()
+      .then(([rows]) => {
+        res.send(rows);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.sendStatus(500);
+      });
+  };
+
+  static read = (req, res) => {
+    models.Opponent.find(req.params.id)
+      .then(([rows]) => {
+        if (rows[0] == null) {
+          res.sendStatus(404);
+        } else {
+          res.send(rows[0]);
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        res.sendStatus(500);
+      });
+  };
+
+  static edit = (req, res) => {
+    const Opponent = req.body;
+
+    // TODO validations (length, format...)
+
+    Opponent.id = parseInt(req.params.id, 10);
+
+    models.Opponent.update(Opponent)
+      .then(([result]) => {
+        if (result.affectedRows === 0) {
+          res.sendStatus(404);
+        } else {
+          res.sendStatus(204);
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        res.sendStatus(500);
+      });
+  };
+
+  static add = (req, res) => {
+    const Opponent = req.body;
+
+    // TODO validations (length, format...)
+
+    models.Opponent.insert(Opponent)
+      .then(([result]) => {
+        res.status(201).send({ ...Opponent, id: result.insertId });
+      })
+      .catch((err) => {
+        console.error(err);
+        res.sendStatus(500);
+      });
+  };
+
+  static delete = (req, res) => {
+    models.Opponent.delete(req.params.id)
+      .then(() => {
+        res.sendStatus(204);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.sendStatus(500);
+      });
+  };
+}
+
+module.exports = OpponentController;
