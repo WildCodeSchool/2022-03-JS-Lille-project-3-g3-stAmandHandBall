fc6d7b925762c78f0128404e0bfe81ad06cd4dd9
NtGZeko <matheooz@live.fr>
2022-07-13 16:21:25 +0200

    finish page all card and role

--- frontend/src/pages/Coachs/index.jsx
+++ frontend/src/pages/Coachs/index.jsx
@@ -3,7 +3,6 @@ import BigPicture from "@components/Archi/BigPicture";
 import BigTitle from "@components/Archi/BigTitle";
 import Title from "@components/Archi/Title";
 import CardStaff from "@components/CardStaff";
-import StaffList from "@components/StaffList";
 import ctxProvider from "@services/context/Ctx";
 import { useContext } from "react";
 import SCoachs from "./style";
@@ -18,25 +17,79 @@ export default function Coachs() {
         <Title title="FILIERE PRO" />
         <section className="rowCard">
           {team
-            .filter((person) => ["Baby"].includes(person.title))
+            .filter((person) => ["LBE"].includes(person.title))
             .map((person) => {
               return (
                 <CardStaff
                   key={person.id}
                   name={`${person.firstname} ${person.lastname}`}
                   img={`./src/assets/images/cards/staff/${person.img}`}
-                  role={person.name}
+                  role={person.title}
                 />
               );
             })}
         </section>
         <Arrow />
         <Title title="FILIERE FILLES" />
+        <section className="rowCard">
+          {team
+            .filter((person) =>
+              [
+                "N1F",
+                "U18F",
+                "N3F",
+                "U17F",
+                "U15F1",
+                "U15F2",
+                "U13F",
+                "U11F",
+              ].includes(person.title)
+            )
+            .map((person) => {
+              return (
+                <CardStaff
+                  key={person.id}
+                  name={`${person.firstname} ${person.lastname}`}
+                  img={`./src/assets/images/cards/staff/${person.img}`}
+                  role={person.title}
+                />
+              );
+            })}
+        </section>
         <Arrow />
         <Title title="FILIERE GARCONS" />
+        <section className="rowCard">
+          {team
+            .filter((person) =>
+              ["U15G", "U13G", "U11G", "SG"].includes(person.title)
+            )
+            .map((person) => {
+              return (
+                <CardStaff
+                  key={person.id}
+                  name={`${person.firstname} ${person.lastname}`}
+                  img={`./src/assets/images/cards/staff/${person.img}`}
+                  role={person.title}
+                />
+              );
+            })}
+        </section>
         <Arrow />
         <Title title="FILIERE ENFANTS" />
-        <StaffList />
+        <section className="rowCard">
+          {team
+            .filter((person) => ["Mini", "Baby"].includes(person.title))
+            .map((person) => {
+              return (
+                <CardStaff
+                  key={person.id}
+                  name={`${person.firstname} ${person.lastname}`}
+                  img={`./src/assets/images/cards/staff/${person.img}`}
+                  role={person.title}
+                />
+              );
+            })}
+        </section>
       </SCoachs>
     </>
   );
