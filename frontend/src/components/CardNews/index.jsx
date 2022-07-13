4a74bc984bcff011c9a7da96124690b079dd7d62
Gonaline <aline-gonce_student2022@wilder.school>
2022-07-13 10:03:52 +0200

    update news route with others pictures and update news page & components

--- frontend/src/components/CardNews/index.jsx
+++ frontend/src/components/CardNews/index.jsx
@@ -1,16 +1,27 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+
 import PropTypes from "prop-types";
 import SCardNews from "./style";
 
-export default function CardNews({ title, content, img, id }) {
+export default function CardNews({ title, content, img1, id }) {
+  const navigate = useNavigate();
+
+  function changeLocation(placeToGo) {
+    navigate(placeToGo, { replace: true });
+    window.location.reload();
+  }
+
   return (
     <SCardNews>
       <div className="mainCards">
         <div className="title">
           <h4>{title}</h4>
         </div>
-        <Link to={`/actus/${id}`}>
-          <img src={`../../src/assets/images/photos/${img}`} alt="imgNews" />
+        <Link
+          to={`/actus/${id}`}
+          onClick={() => changeLocation(`/actus/${id}`)}
+        >
+          <img src={`../../src/assets/images/photos/${img1}`} alt="imgNews" />
         </Link>
         <div className="text">{content}</div>
       </div>
@@ -22,5 +33,5 @@ CardNews.propTypes = {
   id: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
   content: PropTypes.string.isRequired,
-  img: PropTypes.string.isRequired,
+  img1: PropTypes.string.isRequired,
 };
