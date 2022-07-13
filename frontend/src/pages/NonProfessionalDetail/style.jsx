a92134f6dbb4b8a24ad8a1a6474506d5bbbff48f
Gonaline <aline-gonce_student2022@wilder.school>
2022-07-13 14:51:24 +0200

    add find(id) of team, update page & style non Professional

--- frontend/src/pages/NonProfessionalDetail/style.jsx
+++ frontend/src/pages/NonProfessionalDetail/style.jsx
@@ -2,10 +2,33 @@ import styled from "styled-components";
 
 export default styled.div`
   display: flex;
-  flex-wrap: wrap;
-  width: 75vw;
+  flex-direction: column;
+  width: 80vw;
   margin: auto;
+
+  .cardAndPhoto {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    align-items: flex-start;
+    margin-bottom: 10vh;
+
+    .teamImg {
+      width: 70%;
+    }
+  }
+
   @media screen and (max-width: 700px) {
     width: 90vw;
+
+    .cardAndPhoto {
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+
+      .teamImg {
+        width: 100%;
+      }
+    }
   }
 `;
