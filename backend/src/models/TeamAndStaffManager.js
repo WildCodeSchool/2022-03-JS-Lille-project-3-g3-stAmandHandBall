9219d432fccc260b25cf27b968380bdfa35e2585
NtGZeko <matheooz@live.fr>
2022-07-13 15:31:56 +0200

    change SQL request and first posting

--- backend/src/models/TeamAndStaffManager.js
+++ backend/src/models/TeamAndStaffManager.js
@@ -5,7 +5,10 @@ class TeamAndStaffManager extends AbstractManager {
 
   findAll() {
     return this.connection.query(
-      `SELECT s.lastname, s.firstname, s.img, t.title, t.division FROM staff AS s LEFT JOIN team AS t ON t.id=staff_id;`
+      `select *
+      FROM 
+        team 
+          inner join staff on staff.id=team.staff_id;`
     );
   }
 }
--- frontend/src/pages/Coachs/index.jsx
+++ frontend/src/pages/Coachs/index.jsx
@@ -2,16 +2,34 @@ import Arrow from "@components/Archi/Arrow";
 import BigPicture from "@components/Archi/BigPicture";
 import BigTitle from "@components/Archi/BigTitle";
 import Title from "@components/Archi/Title";
+import CardStaff from "@components/CardStaff";
 import StaffList from "@components/StaffList";
+import ctxProvider from "@services/context/Ctx";
+import { useContext } from "react";
 import SCoachs from "./style";
 
 export default function Coachs() {
+  const { team } = useContext(ctxProvider);
   return (
     <>
       <BigPicture img="test" date="" hour="" club1="" club2="" text="" />
       <BigTitle title="Bergers Des Louveteaux" />
       <SCoachs>
         <Title title="FILIERE PRO" />
+        <section className="rowCard">
+          {team
+            .filter((person) => ["Baby"].includes(person.title))
+            .map((person) => {
+              return (
+                <CardStaff
+                  key={person.id}
+                  name={`${person.firstname} ${person.lastname}`}
+                  img={`./src/assets/images/cards/staff/${person.img}`}
+                  role={person.name}
+                />
+              );
+            })}
+        </section>
         <Arrow />
         <Title title="FILIERE FILLES" />
         <Arrow />
