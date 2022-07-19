a7a7dc80ab4e7544b330676461e775de91a29076
romainLF <romainlefloch@hotmail.com>
2022-07-19 11:07:42 +0200

    add new icon

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,6 +1,14 @@
 import { Admin, Resource, EditGuesser } from "react-admin";
 import UserIcon from "@mui/icons-material/Group";
+import WorkIcon from "@mui/icons-material/Work";
+import AdjustIcon from "@mui/icons-material/Adjust";
+import HistoryIcon from "@mui/icons-material/History";
+import GroupsIcon from "@mui/icons-material/Groups";
+import SportsIcon from "@mui/icons-material/Sports";
+import ManageAccountsIcon from "@mui/icons-material/ManageAccounts";
 import FeedIcon from "@mui/icons-material/Feed";
+import AdminPanelSettingsIcon from "@mui/icons-material/AdminPanelSettings";
+import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
 import { PlayerList, PostEdit, PostCreate } from "./player";
 import dataProvider from "./dataProvider";
 import authProvider from "./authProvider";
@@ -44,56 +52,56 @@ function App() {
         list={NewListAdmin}
         edit={EditGuesser}
         create={NewCreateAdmin}
-        icon={FeedIcon}
+        icon={AdminPanelSettingsIcon}
       />
       <Resource
         name="calendar"
         list={NewListCalendar}
         edit={NewEditCalendar}
         create={NewCreateCalendar}
-        icon={FeedIcon}
+        icon={CalendarMonthIcon}
       />
       <Resource
         name="staff"
         list={NewListStaff}
         edit={NewEditStaff}
         create={NewCreateStaff}
-        icon={FeedIcon}
+        icon={ManageAccountsIcon}
       />
       <Resource
         name="role"
         list={NewListRole}
         edit={NewEditRole}
         create={NewCreateRole}
-        icon={FeedIcon}
+        icon={WorkIcon}
       />
       <Resource
         name="referee"
         list={NewListReferee}
         edit={NewEditReferee}
         create={NewCreateReferee}
-        icon={FeedIcon}
+        icon={SportsIcon}
       />
       <Resource
         name="team"
         list={NewListTeam}
         edit={NewEditTeam}
         create={NewCreateTeam}
-        icon={FeedIcon}
+        icon={GroupsIcon}
       />
       <Resource
         name="opponent"
         list={NewListOpponent}
         edit={NewEditOpponent}
         create={NewCreateOpponent}
-        icon={FeedIcon}
+        icon={AdjustIcon}
       />
       <Resource
         name="history"
         list={NewListHistory}
         edit={NewEditHistory}
         create={NewCreateHistory}
-        icon={FeedIcon}
+        icon={HistoryIcon}
       />
     </Admin>
   );
